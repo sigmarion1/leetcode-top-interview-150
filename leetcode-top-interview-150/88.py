ab9e574b70fd37afeaf6ae5aca268c7c18ff7c98
sigmarion1 <axsim@naver.com>
2023-10-04 15:23:24 +0900

    2023-10-04

--- leetcode-top-interview-150/88.py
+++ leetcode-top-interview-150/88.py
@@ -4,4 +4,14 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
 
-        nums1[:] = sorted(nums1[:m] + nums2[:n])
+        i, j, cur = m - 1, n - 1, m + n - 1
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[cur] = nums1[i]
+                i -= 1
+            else:
+                nums1[cur] = nums2[j]
+                j -= 1
+
+            cur -= 1
