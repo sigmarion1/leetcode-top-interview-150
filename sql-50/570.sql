a9aca022943ba6d25c48157c6c9329fa3e4b405d
sigmarion1 <axsim@naver.com>
2023-12-19 22:14:06 +0900

    2023-12-19

---
+++ sql-50/570.sql
@@ -0,0 +1,15 @@
+select
+    name
+from
+    Employee
+where
+    id in (
+        select
+            managerId
+        from
+            Employee
+        group by
+            managerId
+        having
+            count(managerId) >= 5
+    )
